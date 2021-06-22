a319470369c2dc18c79d42a80a5ee903605c6d56
GGunter * <ggunter.game@gmail.com>
2021-06-22 19:59:28 -0300

    Recuperando estado de autenticação

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -4,7 +4,6 @@ import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss';
 import { Button  } from '../components/Button';
