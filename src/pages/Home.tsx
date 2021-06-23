3097f947045bb8b3e2d6d4daee6feac44a3689cb
GGunter * <ggunter.game@gmail.com>
2021-06-23 11:26:28 -0300

    Entrando na sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom'
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -7,11 +8,13 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import '../styles/auth.scss';
 import { Button  } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 
 export function Home(){
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom(){
         if(!user){
@@ -19,6 +22,23 @@ export function Home(){
         }
 
         history.push('/rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent){
+        event?.preventDefault();
+
+        if(roomCode.trim() === ''){
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()){
+            alert('Room does not exists');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
 
     return (
@@ -38,15 +58,18 @@ export function Home(){
                     <div className="separator">
                         ou entre em uma salas
                     </div>
-                    <form action="">
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                         type="text" 
                         placeholder="Digite o código da sala"
+                        onChange={evet => setRoomCode(evet.target.value)}
+                        value={roomCode}
                         />
-                    </form>
-                    <Button type="submit">
+                        <Button type="submit">
                         Entrar na sala
-                    </Button>
+                        </Button>
+                    </form>
+                    
                 </div>
             </main>
         </div>
